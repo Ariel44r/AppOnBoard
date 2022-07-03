44f66fcd4f0e7b9f793f1887e429958c12b57f1f
Ariel <ariel.ramirez@kavak.com>
2022-07-03 11:37:32 -0500

    Initial Commit

---
+++ Shared/AppOnBoardApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppOnBoardApp.swift
+//  Shared
+//
+//  Created by Ariel Ramirez on 03/07/22.
+//
+
+import SwiftUI
+
+@main
+struct AppOnBoardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
